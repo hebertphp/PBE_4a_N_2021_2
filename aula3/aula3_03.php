27e7bdf33a3db6a6526ea9a3b054faa60f21967b
Hebert <hebertphp@gmail.com>
2021-08-25 22:18:59 -0300

    PBE_4a

--- aula3/aula3_03.php
+++ aula3/aula3_03.php
@@ -1,6 +1,6 @@
 <?php
-$x = 1234; //número decimal
-$y = -123; //um número negativo
+$x = 1234; //número decimal positivo
+$y = -123; //número decimal negativo
 echo "<br>x = ".$x;
 echo "<br>y = ".$y;
 ?>
