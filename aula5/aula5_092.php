31b110d3120a3a54f0aac563e3e43d265c4786e2
Hebert <hebertphp@gmail.com>
2021-10-06 20:27:09 -0300

    PBE_4a 06/10

--- aula5/aula5_092.php
+++ aula5/aula5_092.php
@@ -1,6 +1,16 @@
 <?php
 $senha=$_GET["senha"];
-echo $senha;
+// echo $senha;
+$cont=0;
+$cad_senha="123@mudar";
+if($senha==$cad_senha){
+    echo "Acesso ok";
+}
+else{
+    echo "Acesso negado!";
+    $cont++;
+}
+echo "<br>cont = $cont";
 /*
 aula5_092.php?senha=123
 */
