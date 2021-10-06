ab4e918daeb4e3febb1092c8bfaa3490e567896a
Hebert <hebertphp@gmail.com>
2021-10-06 19:55:14 -0300

    PBE_4a 06/10

---
+++ aula5/aula5_081.php
@@ -0,0 +1,33 @@
+<?php
+$i=0;
+while($i<=5){
+    echo "$i <br>";
+    $i++;
+}
+/*
+1) Qual o valor inicial? 0
+2) Qual a condição? $i<=5   < <=  > >=
+3) Qual o contador? $i++
+4) Qual o valor que torna a condição falsa? 6
+5) Qual foi o último valor impresso. 5
+
+
+Desafio 1)
+Saída
+7
+8
+9
+10
+
+Desafio 2)
+Saída
+2
+1
+0
+
+Desafio 3)
+3
+5
+7
+9
+*/
---
+++ aula5/aula5_091.php
@@ -0,0 +1,11 @@
+<?php
+$i=0; // inicialização do nosso contador;
+do {
+  echo $i . "<br> ";
+  $i++; // incremento do contador
+} while ($i < 5);
+/*
+
+
+*/
+?>
