086402d66b68f747347a1685c47b2107dc290281
Hebert <hebertphp@gmail.com>
2021-10-06 22:21:06 -0300

    PBE_4a 06/10

---
+++ aula6/aula6_011.php
@@ -0,0 +1,11 @@
+<?php
+$i[]="letras de a - g";//i[] índice ou pos 0
+$i[2]="letras de h - q";
+$i[]="letras de r - z";
+$i[3]="E agora???";
+
+$alunos=array("Astrogildo","Belarmina","Abelardo","Pafuncio");
+//                [0]          [1]        [2]        [3]
+//echo $alunos[2];
+echo "<pre>";print_r($i);echo "</pre>";
+?>
