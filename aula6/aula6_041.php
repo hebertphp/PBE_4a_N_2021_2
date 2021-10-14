43d09018f82f51592c942de91721689f3defb47c
Hebert <hebertphp@gmail.com>
2021-10-13 21:41:33 -0300

    PBE_4a 13/10/2021

---
+++ aula6/aula6_041.php
@@ -0,0 +1,13 @@
+<?php
+$aluno=array();//var do tipo array vazia
+$aluno["curso"] = "TADS";
+$aluno["nome"] = "Fulano";
+$aluno["ra"] = 123456789;
+$aluno[]="E agora???";//qual a posição? 0
+//$aluno=array();//apaga o array!!!
+
+echo "<pre>";print_r($aluno);echo "</pre>";
+echo "<hr>Nome:-- <b>{$aluno["nome"]} </b>";
+echo "<br>Ra: ".$aluno["ra"];
+echo "<br>Curso: ".$aluno["curso"];
+?>
