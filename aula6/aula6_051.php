a1dd4937d889bc79d0cff30ad31fe95bee935514
Hebert <hebertphp@gmail.com>
2021-10-13 22:05:33 -0300

    PBE_4a 13/10/2021

--- aula6/aula6_051.php
+++ aula6/aula6_051.php
@@ -1,8 +1,5 @@
 <?php
-$aluno = array ( 
- "curso" => "TADS",
- "nome"  => "Fulano",
- "ra"    => 123456789 );
+$aluno = array ("curso" => "TADS","nome" =>"Fulano","ra"=> 123456789);
 
 echo "<pre>";print_r($aluno);echo "</pre>";
 echo "<hr>Nome: ".$aluno["nome"];
